bb3ec331756fba986aecfa1cb9427a0e30f7db7a
nagarajar <nagarajar70@gmail.com>
2025-08-31 16:22:00 +0530

    Bubble Sort with time and space complexity and optimization

---
+++ src/practice/dsa/java/a/intro/BubbleSort.java
@@ -0,0 +1,57 @@
+package practice.dsa.java.a.intro;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] a = { 91, 21, 31, 10, 82, 5 };
+		int[] a1 = { 5, 21, 31, 50, 82, 91 }; //In case if the array is already swapped we don't have compare all elements
+
+		bubbleSort(a);
+		System.out.println("***********************************");
+		bubbleSortOptimised(a1);
+	}
+
+	private static void bubbleSort(int[] a) {
+		// TODO Auto-generated method stub
+		int n=a.length;
+		for(int i=0; i< n-1; i++) {	// We don't have to travel till the n, we go worst case till n-1
+			for(int j=0; j<n-i-1; j++) { // Each iteration the last element will sort so no need compare again the sorted elements
+				if(a[j]>a[j+1]) {
+					int temp = a[j];
+					a[j] = a[j+1];
+					a[j+1]=temp;
+				}
+			}
+			System.out.println("i value "+i);//debug
+		}
+		System.out.println(Arrays.toString(a));
+	}
+	
+	private static void bubbleSortOptimised(int[] a) {
+		// TODO Auto-generated method stub
+		int n=a.length;
+		for(int i=0; i< n-1; i++) {
+			boolean swapped = false;
+			for(int j=0; j<n-i-1; j++) {
+				if(a[j]>a[j+1]) {
+					int temp = a[j];
+					a[j] = a[j+1];
+					a[j+1]=temp;
+					swapped = true;
+				}
+			}
+			if(!swapped) { // If no elements are swapped means its already sorted to break the loop
+				System.out.println("i value "+i);//debug
+				break;
+			}
+		}
+		System.out.println(Arrays.toString(a));
+	}
+	
+	//Time Complexity = Worst case --> O(n^2) and best case with Optimization = O(n)
+	//Space Complexity (Auxiliary space/memory) = O(1)
+
+}
